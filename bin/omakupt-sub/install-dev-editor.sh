43821bae54be60119412d725024ab45d64dce058
Philipp Tuchardt <post@pt1602.de>
2026-09-14 15:53:41 +0200

    feat(installer): enhance editor installation with PhpStorm-specific logic

--- bin/omakupt-sub/install-dev-editor.sh
+++ bin/omakupt-sub/install-dev-editor.sh
@@ -3,20 +3,25 @@
 CHOICES=(
   "Cursor            AI Code Editor"
   "Doom Emacs        Emacs framework with curated list of packages"
+  "PhpStorm          JetBrains' commercial PHP editor (installed by default, use to reinstall/update)"
   "RubyMine          IntelliJ's commercial Ruby editor"
   "Windsurf          Another AI Code Editor"
   "Zed               Fast all-purpose editor"
   "<< Back           "
 )
 
-CHOICE=$(gum choose "${CHOICES[@]}" --height 8 --header "Install editor")
+CHOICE=$(gum choose "${CHOICES[@]}" --height 9 --header "Install editor")
 
 if [[ "$CHOICE" == "<< Back"* ]] || [[ -z "$CHOICE" ]]; then
   # Don't install anything
   echo ""
 else
   INSTALLER=$(echo "$CHOICE" | awk -F ' {2,}' '{print $1}' | tr '[:upper:]' '[:lower:]' | sed 's/ /-/g')
-  INSTALLER_FILE="$OMAKUPT_PATH/install/desktop/optional/app-$INSTALLER.sh"
+
+  case "$INSTALLER" in
+  "phpstorm") INSTALLER_FILE="$OMAKUPT_PATH/install/desktop/app-phpstorm.sh" ;;
+  *) INSTALLER_FILE="$OMAKUPT_PATH/install/desktop/optional/app-$INSTALLER.sh" ;;
+  esac
 
   source $INSTALLER_FILE && gum spin --spinner globe --title "Install completed!" -- sleep 3
 fi
